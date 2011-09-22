0620da4ee74102fcd24c307bddfc5d94d59ed138
hungryblank <hungryblank@gmail.com>
2011-09-22 13:10:01 -0700

    add skeleton

---
+++ src/dejadb_app.erl
@@ -0,0 +1,16 @@
+-module(dejadb_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    dejadb_sup:start_link().
+
+stop(_State) ->
+    ok.
